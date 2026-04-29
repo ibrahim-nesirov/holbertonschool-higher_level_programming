55e6b4c4d2413a022502681ea1fea11a59e00ebe
ibrahim-nesirov <inesirov778@gmail.com>
2026-04-30 00:46:38 +0400

    ,

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Python is an interpreted, interactive, object-oriented programming language that is named after Monty Python's Flying Circus"
+str2 = "Python designed by Guido van Rossum"
+print(str1[39:67] + str2[7:11] + str1[0:6])
