0efe5a4e9e25518c12a9ae47f1c1b37eff43247c
ibrahim-nesirov <inesirov778@gmail.com>
2026-04-30 10:51:15 +0400

    1

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
