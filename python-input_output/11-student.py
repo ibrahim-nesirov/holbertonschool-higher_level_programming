9d73c2e6bcc770f5706fa844df22402fb57f253b
ibrahim-nesirov <inesirov778@gmail.com>
2026-04-30 11:23:44 +0400

    1

---
+++ python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for Student class."""
+
+
+class Student:
+    """A class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student with first_name, last_name and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dictionary representation of Student.
+
+        Args:
+            attrs (list): List of attribute names to retrieve.
+
+        Returns:
+            dict: Dictionary representation of the student.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of Student instance.
+
+        Args:
+            json (dict): Dictionary of attributes to set.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
