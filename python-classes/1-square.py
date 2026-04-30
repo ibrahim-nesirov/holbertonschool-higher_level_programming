20e0046a3d338a6c44afdedf2a7fb58a5aef7ebb
ibrahim-nesirov <inesirov778@gmail.com>
2026-04-30 10:47:43 +0400

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize square with size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
+        
