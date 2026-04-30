de457d91e661387bb60e4df371f5cfabd21fc1ff
ibrahim-nesirov <inesirov778@gmail.com>
2026-04-30 10:58:02 +0400

    1

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation.
+
+        Args:
+            value (int): The size value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
