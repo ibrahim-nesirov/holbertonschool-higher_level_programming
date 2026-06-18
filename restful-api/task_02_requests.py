787d45665d118758ed43cb393960952b227b1963
ibrahim nesirov <inesirov778@gmail.com>
2026-06-19 00:25:38 +0400

    dd

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,25 +1,39 @@
+#!/usr/bin/python3
+"""This module contains restful-api tasks."""
 import requests
 import csv
+import json
 
 
 def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {response.status_code}")
-    if response.status_code == 200:
-        posts = response.json()
-        for post in posts:
-            print(post["title"])
+    """Function to fetch and print posts."""
 
+    resp = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    sc = resp.status_code
+    r_json = resp.json()
+    
+    print(f"Status Code: {sc}")
+
+    for i in r_json:
+        print(i["title"])
 
 def fetch_and_save_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if response.status_code == 200:
-        posts = response.json()
-        structured = [
-            {"id": post["id"], "title": post["title"], "body": post["body"]}
-            for post in posts
-        ]
-        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=["id", "title", "body"])
+    """Function to fetch and save posts."""
+
+    resp = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    sc = resp.status_code
+    header = ["id", "title", "body"]
+    r_json = resp.json()
+
+    if (sc == 200):
+        with open("posts.csv", "w") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
             writer.writeheader()
-            writer.writerows(structured)
+            
+            for post in r_json:
+                row = {}
+                for field in header:
+                    row[field] = post[field]
+                    writer.writerow(row)
